0ca003e30cc1422a99b3892406e921c712a6907e
Heesakkers,Aron A.P.H.M <aron.heesakkers@student.fontys.nl>
2021-04-08 13:49:18 +0000

    added package controller etc.
    TODO: support localdb for testing

---
+++ Database/Contexts/PackageServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PakketService.Database.Datamodels;
+
+namespace PakketService.Database.Contexts
+{
+    public class PackageServiceContext : DbContext
+    {
+        public PackageServiceContext (DbContextOptions<PackageServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Package> Package { get; set; }
+    }
+}
---
+++ Database/Datamodels/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PakketService.Database.Datamodels
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string ToDoLocationId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+        public string CreatedByPCN { get; set; }
+        public double FinishedAt { get; set; }
+        public string FinishedByPCN { get; set; }
+        public bool IsFinished { get; set; }
+        public string NextTicketId { get; set; }
+        public TicketAction TicketAction { get; set; }
+    }
+}
