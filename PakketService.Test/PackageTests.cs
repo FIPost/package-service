99c5c172cb367d89d4be9f1c28bbe7ac81f94d23
Sverre van Gompel <sverrevangompel@hotmail.com>
2021-04-29 16:46:42 +0200

    Fixed an issue where test project was not included

---
+++ PakketService.Test/PackageTests.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using PakketService.Controllers;
+using PakketService.Database.Converters;
+using PakketService.Database.Datamodels;
+using PakketService.Database.Datamodels.Dtos;
+using PakketService.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace PakketService.Test
+{
+    public class Tests
+    {
+        private Mock<IPackageService> serviceMock;
+        private Mock<IDtoConverter<Package, PackageRequest, PackageResponse>> converterMock;
+
+        private Package package;
+        private PackageRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Instantiate mocks:
+            serviceMock = new Mock<IPackageService>();
+            converterMock = new Mock<IDtoConverter<Package, PackageRequest, PackageResponse>>();
+
+            // Create mock data:
+            package = new Package
+            {
+                Sender = "Coolblue",
+                ReceiverId = new Guid(),
+                Name = "Test",
+                CollectionPointId = new Guid()
+            };
+            request = new PackageRequest
+            {
+                Sender = "Coolblue",
+                ReceiverId = new Guid(),
+                Name = "Test",
+                CollectionPointId = new Guid()
+            };
+        }
+
+        [Test]
+        public async Task AddPackageTest()
+        {
+            // Arrange
+            serviceMock.Setup(x => x.AddAsync(package)).Returns(Task.FromResult(package));
+            var controller = new PackagesController(serviceMock.Object, converterMock.Object);
+
+            // Act
+            var result = await controller.AddPackage(request);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ActionResult<PackageResponse>>(result);
+        }
+
+        [Test]
+        public async Task GetPackageByIdTest()
+        {
+            // Arrange
+            serviceMock.Setup(x => x.GetByIdAsync(new Guid())).Returns(Task.FromResult(package));
+            var controller = new PackagesController(serviceMock.Object, converterMock.Object);
+
+            // Act
+            var result = await controller.GetPackageById(new Guid());
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ActionResult<PackageResponse>>(result);
+        }
+    }
+}
