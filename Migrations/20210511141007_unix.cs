201973cfb51295b1d82ae118a37ae08592bcf853
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-05-11 17:41:53 +0200

    migration added

---
+++ Migrations/20210511141007_unix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PakketService.Migrations
+{
+    public partial class unix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Ticket");
+
+            migrationBuilder.AddColumn<double>(
+                name: "CreatedAt",
+                table: "Ticket",
+                type: "float",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+              name: "CreatedAt",
+              table: "Ticket");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Ticket",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: DateTime.Now);
+        }
+    }
+}
