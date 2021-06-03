f70cf02172d97802019fa0483b110e3242854e76
Jaap van der Meer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-06-03 11:17:27 +0200

    #20 pakket levering (#18)
    * start refactor models
    
    * migrations added
    
    * ticket post request validation added
    
    * ticketresponse update
    
    * tickets sorted by newest date
    
    * ticket controller route update

---
+++ Services/TicketService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PakketService.Database.Contexts;
+using PakketService.Database.Converters;
+using PakketService.Database.Datamodels;
+using PakketService.Database.Datamodels.Dtos;
+using PakketService.helpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PakketService.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly PackageServiceContext _context;
+        private readonly IDtoConverter<Ticket, TicketRequest, TicketResponse> _converter;
+
+        public TicketService(PackageServiceContext context, IDtoConverter<Ticket, TicketRequest, TicketResponse> converter)
+        {
+            _context = context;
+            _converter = converter;
+        }
+
+        public async Task<TicketResponse> AddAsync(TicketRequest request)
+        {
+            Package package = await _context.Package.FirstOrDefaultAsync(p => p.Id == request.PackageId);
+            if (package == null)
+            {
+                throw new NotFoundException($"Package with id {request.PackageId} not found.");
+            }
+
+            Ticket ticket = _converter.DtoToModel(request);
+            ticket.FinishedAt = DateTimeOffset.Now.ToUnixTimeSeconds();
+            await _context.AddAsync(ticket);
+
+            if (ticket.LocationId == package.CollectionPointId)
+            {
+                package.RouteFinished = true;
+                _context.Update(package);
+            }
+
+            await _context.SaveChangesAsync();
+            return _converter.ModelToDto(ticket);
+        }
+    }
+}
