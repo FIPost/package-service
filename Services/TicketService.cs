d08d8b8dbed6b0cdb64c3eb40d084f52a86e90b9
SamPertijs <pertijssam@gmail.com>
2021-09-30 14:44:39 +0200

    Package can't be sent to same location twice in a row anymore. Logic check in ticketservice

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -29,6 +29,15 @@ namespace PakketService.Services
                 throw new NotFoundException($"Package with id {request.PackageId} not found.");
             }
 
+            var a = _context.Ticket
+                .Where(t => t.PackageId == request.PackageId)
+                .OrderByDescending(t => t.FinishedAt).ToList();
+            if(a.FirstOrDefault().LocationId == request.LocationId)
+            {
+                throw new NotFoundException($"Package with id {request.PackageId} not found. (test for same locId)");
+            }
+
+
             Ticket ticket = _converter.DtoToModel(request);
             ticket.FinishedAt = DateTimeOffset.Now.ToUnixTimeSeconds();
             await _context.AddAsync(ticket);
